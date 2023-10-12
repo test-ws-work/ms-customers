5f4819704aa61a0699c8865068c958e6a398e27e
gusferreira <gustavo.ferreira@nutrip.com.br>
2023-10-12 19:32:14 -0300

    chore: initial config

---
+++ src/test/java/br/com/wswork/module/customers/MsCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wswork.module.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
